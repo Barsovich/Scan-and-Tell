da5c8f468cbc2d5a98c788f38c872a1b4604040a
Baris Sen <baris.sen@tum.de>
2020-12-06 14:31:13 +0100

    Move scans to the correct folder

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,4 +1,6 @@
 pip install gdown
+cd ../data/scannet
 gdown https://drive.google.com/uc?id=11I5dQQSuEqEUz1f76jcZ9hR9RcB6aNjI
 tar -xf scans_small.tar.gz
 rm scans_small.tar.gz
+mv scans_small scans
