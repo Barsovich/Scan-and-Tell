8d462eddd4053647f7da22f24c8701537e11ee12
Baris Sen <baris.sen@tum.de>
2020-12-06 23:46:41 +0100

    Add the list of all scans for the short version

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,3 +1,4 @@
+source activate scan-and-tell
 pip install gdown
 cd ../data/scannet
 gdown https://drive.google.com/uc?id=1zi6TBiVTqXaMJv28eDmKUtviA-mipZUb
@@ -7,6 +8,8 @@ mv scans_small scans
 rm meta_data/scannetv2_val.txt
 rm meta_data/scannetv2_test.txt
 rm meta_data/scannetv2_train.txt
+rm meta_data/scannetv2.txt
 mv meta_data/scannetv2_val_small.txt meta_data/scannetv2_val.txt
 mv meta_data/scannetv2_train_small.txt meta_data/scannetv2_train.txt
 mv meta_data/scannetv2_test_small.txt meta_data/scannetv2_test.txt
+mv meta_data/scannetv2_small.txt meta_data/scannetv2.txt
