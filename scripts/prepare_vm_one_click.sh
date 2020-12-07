faff82d6fea22e0eb31c6eea671184aaf106823b
Baris Sen <baris.sen@tum.de>
2020-12-07 10:48:39 +0100

    Require model in the prepare_vm script

--- scripts/prepare_vm_one_click.sh
+++ scripts/prepare_vm_one_click.sh
@@ -1,3 +1,12 @@
+if [ "$1" != "--model" ]
+then 
+    echo "Please specify the model using --model <model> or -m <model>."
+    exit 1
+else
+    model="$2"
+    echo "Building the project for the model ${model}"
+fi
+
 wget https://raw.githubusercontent.com/Barsovich/Scan-and-Tell/main/scripts/setup.sh
 wget https://raw.githubusercontent.com/Barsovich/Scan-and-Tell/main/scripts/common.sh
 chmod +x setup.sh
@@ -9,4 +18,4 @@ chmod +x scripts/download_data.sh
 cd scripts
 ./download_data.sh
 cd ../data/scannet
-python batch_load_scannet_data.py --model $1
+python batch_load_scannet_data.py --model $model
