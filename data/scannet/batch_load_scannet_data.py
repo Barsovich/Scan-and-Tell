9fec5bcdf25ff47ed6d5bb6ec31342f3c36de472
acerdur <can.erdur@tum.de>
2020-12-08 16:37:51 +0100

    change class id

--- data/scannet/batch_load_scannet_data.py
+++ data/scannet/batch_load_scannet_data.py
@@ -23,7 +23,7 @@ SCANNET_DIR = 'scans'
 SCAN_NAMES = sorted([line.rstrip() for line in open('meta_data/scannetv2.txt')])
 LABEL_MAP_FILE = 'meta_data/scannetv2-labels.combined.tsv'
 DONOTCARE_CLASS_IDS = np.array([])
-OBJ_CLASS_IDS = np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]) # exclude wall (1), floor (2), ceiling (22)
+OBJ_CLASS_IDS = np.array([3,4,5,6,7,8,9,10,11,12,14,16,24,28,33,34,36,39]) #np.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]) # exclude wall (1), floor (2), ceiling (22)
 MAX_NUM_POINT = 50000
 OUTPUT_FOLDER = './scannet_data'
 
--- data/scannet/scannet_utils.py
+++ data/scannet/scannet_utils.py
@@ -129,13 +129,12 @@ def get_labels(filename,IDs):
 
     return sem_labels
 
-g_label_names = ['unannotated', 'wall', 'floor', 'chair', 'table', 'desk', 'bed', 'bookshelf', 'sofa', 'sink', 'bathtub', 'toilet', 'curtain', 'counter', 'door', 'window', 'shower curtain', 'refridgerator', 'picture', 'cabinet', 'otherfurniture']
+g_label_names = ['cabinet','bed','chair','sofa','table','door','window','bookshelf','picture','counter','desk','curtain','refrigerator','shower curtain','toilet','sink','bathtub','others']
+nyu40_ids = np.array([3,4,5,6,7,8,9,10,11,12,14,16,24,28,33,34,36,39])
 
 def get_raw2scannetv2_label_map():
     lines = [line.rstrip() for line in open(os.path.join('meta_data', 'scannetv2-labels.combined.tsv'))]
     lines_0 = lines[0].split('\t')
-    print(lines_0)
-    print(len(lines))
     lines = lines[1:]
     raw2scannet = {}
     for i in range(len(lines)):
@@ -145,10 +144,12 @@ def get_raw2scannetv2_label_map():
         if (elements[1] != elements[2]):
             print('{}: {} {}'.format(i, elements[1], elements[2]))
         nyu40_name = elements[7]
-        if nyu40_name not in label_classes_set:
-            raw2scannet[raw_name] = 'unannotated'
-        else:
-            raw2scannet[raw_name] = nyu40_name
+        nyu40_id = int(elements[4])
+        if nyu40_id in nyu40_ids:
+            if nyu40_name not in label_classes_set:
+                raw2scannet[raw_name] = 'others'
+            else:
+                raw2scannet[raw_name] = nyu40_name
     return raw2scannet
 
 g_raw2scannetv2 = get_raw2scannetv2_label_map()
