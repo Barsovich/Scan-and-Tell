d25ab900d5f92722116b66418c0561c3f1d42181
acerdur <can.erdur@tum.de>
2021-01-15 16:49:09 +0100

    prevent pointgroup logger to stuff when using votene

--- utils/log.py
+++ utils/log.py
@@ -24,17 +24,20 @@ def create_logger(log_file):
     logging.basicConfig(level=logging.DEBUG, format=log_format, filename=log_file)    # filename: build a FileHandler
     return logger
 
-if cfg.task == 'train':
-    log_file = os.path.join(
-        cfg.exp_path,
-        'train-{}.log'.format(time.strftime("%Y%m%d_%H%M%S", time.localtime()))
-    )
-elif cfg.task == 'test':
-    log_file = os.path.join(
-        cfg.exp_path, 'result', 'epoch{}_nmst{}_scoret{}_npointt{}'.format(cfg.test_epoch, cfg.TEST_NMS_THRESH, cfg.TEST_SCORE_THRESH, cfg.TEST_NPOINT_THRESH),
-        cfg.split, 'test-{}.log'.format(time.strftime("%Y%m%d_%H%M%S", time.localtime()))
-    )
-if not os.path.exists(os.path.dirname(log_file)):
-    os.makedirs(os.path.dirname(log_file), exist_ok=True)
-logger = create_logger(log_file)
-logger.info('************************ Start Logging ************************')
+if cfg.model_name == 'pointgroup':
+    if cfg.task == 'train':
+        log_file = os.path.join(
+            cfg.exp_path,
+            'train-{}.log'.format(time.strftime("%Y%m%d_%H%M%S", time.localtime()))
+        )
+    elif cfg.task == 'test':
+        log_file = os.path.join(
+            cfg.exp_path, 'result', 'epoch{}_nmst{}_scoret{}_npointt{}'.format(cfg.test_epoch, cfg.TEST_NMS_THRESH, cfg.TEST_SCORE_THRESH, cfg.TEST_NPOINT_THRESH),
+            cfg.split, 'test-{}.log'.format(time.strftime("%Y%m%d_%H%M%S", time.localtime()))
+        )
+    if not os.path.exists(os.path.dirname(log_file)):
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
+    logger = create_logger(log_file)
+    logger.info('************************ Start Logging ************************')
+else:
+    pass
