3b76c1498b73893ba62b86c9f6d2ef5fc6162980
Baris Sen <baris.sen@tum.de>
2021-01-19 04:27:56 +0100

    Update pointgroup.py to enable feature extraction

--- models/pointgroup.py
+++ models/pointgroup.py
@@ -107,13 +107,59 @@ class UBlock(nn.Module):
 
         return output
 
+class UBlockAsymmetric(nn.Module):
+    def __init__(self, nPlanes, norm_fn, block_reps, block, indice_key_id=1):
+
+        super().__init__()
+
+        self.nPlanes = nPlanes
+
+        blocks = {'block{}'.format(i): block(nPlanes[0], nPlanes[0], norm_fn, indice_key='subm{}'.format(indice_key_id)) for i in range(block_reps)}
+        blocks = OrderedDict(blocks)
+        self.blocks = spconv.SparseSequential(blocks)
+
+        if len(nPlanes) > 1:
+            self.conv = spconv.SparseSequential(
+                norm_fn(nPlanes[0]),
+                nn.ReLU(),
+                spconv.SparseConv3d(nPlanes[0], nPlanes[1], kernel_size=2, stride=2, bias=False, indice_key='spconv{}'.format(indice_key_id))
+            )
+
+            self.u = UBlock(nPlanes[1:], norm_fn, block_reps, block, indice_key_id=indice_key_id+1)
+
+            self.deconv = spconv.SparseSequential(
+                norm_fn(nPlanes[1]),
+                nn.ReLU(),
+                spconv.SparseInverseConv3d(nPlanes[1], nPlanes[0], kernel_size=2, bias=False, indice_key='spconv{}'.format(indice_key_id))
+            )
+
+            blocks_tail = {}
+            for i in range(block_reps):
+                blocks_tail['block{}'.format(i)] = block(nPlanes[0] * 2, nPlanes[0] * 2, norm_fn, indice_key='subm{}'.format(indice_key_id))
+            blocks_tail = OrderedDict(blocks_tail)
+            self.blocks_tail = spconv.SparseSequential(blocks_tail)
+
+    def forward(self, input):
+        output = self.blocks(input)
+        identity = spconv.SparseConvTensor(output.features, output.indices, output.spatial_shape, output.batch_size)
+
+        if len(self.nPlanes) > 1:
+            output_decoder = self.conv(output)
+            output_decoder = self.u(output_decoder)
+            output_decoder = self.deconv(output_decoder)
+
+            output.features = torch.cat((identity.features, output_decoder.features), dim=1)
+
+            output = self.blocks_tail(output)
+
+        return output
+
 
 class PointGroup(nn.Module):
     def __init__(self, cfg):
         super().__init__()
 
         input_c = cfg.input_channel
-        m = cfg.m
         classes = cfg.classes
         block_reps = cfg.block_reps
         block_residual = cfg.block_residual
@@ -151,11 +197,12 @@ class PointGroup(nn.Module):
             input_c += 128
 
         #### backbone
+        m = 32
+        k = 16
         self.input_conv = spconv.SparseSequential(
             spconv.SubMConv3d(input_c, m, kernel_size=3, padding=1, bias=False, indice_key='subm1')
         )
-
-        self.unet = UBlock([m, 2*m, 3*m, 4*m, 5*m, 6*m, 7*m], norm_fn, block_reps, block, indice_key_id=1)
+        self.unet = UBlock([m, 2*k, 3*k, 4*k, 5*k, 6*k, 7*k], norm_fn, block_reps, block, indice_key_id=1)
 
         self.output_layer = spconv.SparseSequential(
             norm_fn(m),
@@ -174,19 +221,15 @@ class PointGroup(nn.Module):
         self.offset_linear = nn.Linear(m, 3, bias=True)
 
         #### score branch
-        self.score_unet = UBlock([m, 2*m], norm_fn, 2, block, indice_key_id=1)
+        self.score_unet = UBlockAsymmetric([m, 2*m], norm_fn, 2, block, indice_key_id=1)
+
         self.score_outputlayer = spconv.SparseSequential(
-            norm_fn(m),
+            norm_fn(2*m),
             nn.ReLU()
         )
-        self.object_features_linear = nn.Sequential(
-            nn.Linear(m, 4*m, bias=True),
-            norm_fn(4*m),
-            nn.ReLU(),
-            nn.Linear(4*m, 8*m, bias=True)
-        )
+        self.object_features_linear = nn.Linear(2*m, 4*m, bias=True)
 
-        self.score_linear = nn.Linear(m, 1)
+        self.score_linear = nn.Linear(2*m, 1)
 
         self.apply(self.set_bn_init)
 
@@ -321,7 +364,7 @@ class PointGroup(nn.Module):
 
         if(epoch > self.prepare_epochs):
             #### get prooposal clusters
-            object_idxs = torch.nonzero(semantic_preds > 1).view(-1)
+            object_idxs = torch.nonzero(semantic_preds >= 0).view(-1)
 
             batch_idxs_ = batch_idxs[object_idxs]
             batch_offsets_ = utils.get_batch_offsets(batch_idxs_, input_.batch_size)
